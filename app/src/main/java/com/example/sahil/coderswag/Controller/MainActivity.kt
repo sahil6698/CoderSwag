cf89edec344669980f0de5016d464fb60db7182b
sahil6698 <emailtosahil@yahoo.com>
2018-04-02 23:49:09 +0530

    L61Commit

--- app/src/main/java/com/example/sahil/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/sahil/coderswag/Controller/MainActivity.kt
@@ -2,12 +2,23 @@ package com.example.sahil.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Adapter
+import android.widget.ArrayAdapter
+import com.example.sahil.coderswag.Model.Category
 import com.example.sahil.coderswag.R
+import com.example.sahil.coderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var adapter:ArrayAdapter<Category>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter= ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                DataService.categories
+                )
+        categoryListView.adapter=adapter
     }
 }
--- app/src/main/java/com/example/sahil/coderswag/Model/Category.kt
+++ app/src/main/java/com/example/sahil/coderswag/Model/Category.kt
@@ -1,3 +1,9 @@
 package com.example.sahil.coderswag.Model
 
-class Category constructor(val title:String, val image:String)
+class Category constructor(val title:String, val image:String){
+    override fun toString(): String {
+        return title
+    }
+
+
+}
